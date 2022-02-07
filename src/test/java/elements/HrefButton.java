ddde407d6f9c8614023ded076afff50ca8be8172
alexandr <a.soloviev@icol.com>
2022-02-07 20:51:31 +0300

    Sasha added profile tests

--- src/test/java/elements/HrefButton.java
+++ src/test/java/elements/HrefButton.java
@@ -4,19 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HrefButton {
-
     WebDriver driver;
     String text;
 
-    String hrefButtonLocator = "//a[text()='%s']";
+    String hrefLocator = "//a[text()='%s']";
 
-
-    public void click(){
-        driver.findElement(By.xpath(String.format(hrefButtonLocator, text))).click();
+    public void click() {
+        driver.findElement(By.xpath(String.format(hrefLocator, text))).click();
     }
 
     public HrefButton(WebDriver driver, String text) {
         this.driver = driver;
         this.text = text;
     }
+
 }
